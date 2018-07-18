f571677fe03e3b37fa0586dc8e771a7c4be75c5c
Johnny Mariéthoz <Johnny.Mariethoz@rero.ch>
2018-07-18 11:12:43 +0200

    api: the state diagram as a png file, closes #31
    Signed-off-by: Johnny Mariéthoz <Johnny.Mariethoz@rero.ch>

---
+++ invenio_circulation/cli.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2018 CERN.
+#
+# Invenio-Circulation is free software; you can redistribute it and/or modify
+# it under the terms of the MIT License; see LICENSE file for more details.
+
+
+"""Click command-line interface for circulation management."""
+
+from __future__ import absolute_import, print_function
+
+import click
+
+
+@click.group()
+def circulation():
+    """Records management."""
+
+
+@circulation.command()
+@click.argument('output_file_name')
+def diagram(output_file_name):
+    """Save the circulation state diagram to a png file."""
+    from .api import Loan
+    Loan.save_diagram(output_file_name)
