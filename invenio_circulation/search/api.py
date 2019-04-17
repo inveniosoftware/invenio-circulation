2eb9daa658332a976b7d5d84bcf379d30cd5416b
vladbmihaescu <vladbmihaescu@protonmail.ch>
2019-04-17 17:41:50 +0200

    search-api: function for performing elasticsearch aggregations on loans

--- invenio_circulation/search/api.py
+++ invenio_circulation/search/api.py
@@ -78,3 +78,56 @@ def search_by_patron_pid(patron_pid):
     search = current_circulation.loan_search
     search = search.filter("term", patron_pid=patron_pid)
     return search
+
+
+def search_and_aggregate(top_agg_name=None,
+                         top_agg_field_name=None,
+                         agg_type="terms",
+                         sub_aggs=None,
+                         filter_states=None, exclude_states=None,
+                         filters=None,
+                         sort_by_field=None, sort_order="asc",
+                         max_number_of_results=None):
+    """Perform aggregation on all loans.
+
+    Parameters:
+        top_agg_name (str): The top level bucket name
+        top_agg_field_name (str): The bucket criterion
+        agg_type (str): The type of aggregation to be performed
+        sub_aggs (list): A list of dictionaries, each containing
+                         the key, value pair of bucket name and criterion
+        filters (list): A list of dictionaries, each containing the key, value
+                        pair of (dotted) field names on which to filter
+                        and a list of values
+
+    """
+    search = current_circulation.loan_search
+    kwargs = dict()
+
+    if filter_states:
+        search = search.filter("terms", state=filter_states)
+    elif exclude_states:
+        search = search.filter("terms", state=exclude_states)
+
+    if filters:
+        for query_filter in filters:
+            search = search.filter("terms", **query_filter)
+
+    kwargs.update({"field": top_agg_field_name})
+    if sort_by_field:
+        kwargs.update({"order": {sort_by_field: sort_order}})
+    if max_number_of_results:
+        kwargs.update({"size": max_number_of_results})
+
+    search.aggs.bucket(top_agg_name,
+                       agg_type,
+                       **kwargs)
+
+    if sub_aggs:
+        for sub_agg in sub_aggs:
+            kwargs.update({"field": sub_agg["field_name"]})
+            search.aggs[top_agg_name].bucket(sub_agg["name"],
+                                             agg_type,
+                                             **kwargs)
+
+    return search.execute()
